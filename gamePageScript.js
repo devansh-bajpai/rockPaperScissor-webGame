88b9c8d70c0a5818797067aaa6efb5067380e33e
Devansh Bajpai <devanshb.code@gmail.com>
2024-12-28 21:20:44 +0530

    added second page too

--- gamePageScript.js
+++ gamePageScript.js
@@ -1,3 +1,14 @@
+function tossButtonClicked() {
+    let tossImgDiv = document.body.querySelector("#tossImgDiv");
+    tossImgDiv.style.animationName = "rotateClock"
+    tossImgDiv.style.animationDuration = "1s";
+
+    let tossButton = document.body.querySelector("#tossButton");
+    tossButton.disabled = true;
+
+    
+}
+
 
 document.body.onload = setTimeout(() => {
     let toHide = document.body.querySelectorAll(".toBeHiddenAfterLoad");
