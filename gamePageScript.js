a8ad999a272462544ff65b09868b01e13de0127f
Devansh Bajpai <devanshb.code@gmail.com>
2024-12-28 23:47:56 +0530

    added choice options

--- gamePageScript.js
+++ gamePageScript.js
@@ -1,19 +1,20 @@
-function tossButtonClicked() {
-    let tossImgDiv = document.body.querySelector("#tossImgDiv");
-    tossImgDiv.style.animationName = "rotateClock"
-    tossImgDiv.style.animationDuration = "1s";
+function afterLoad() {
+    let gameWindow = document.body.querySelector("#gameWindow");
+    // gameWindow.style.display = 'unset';
 
-    let tossButton = document.body.querySelector("#tossButton");
-    tossButton.disabled = true;
-
-    
+    gameWindow.style.display = 'flex';
+    gameWindow.style.alignItems = 'center';
+    gameWindow.style.justifyContent = 'center';
+    gameWindow.style.flexDirection = 'column';
 }
-
 
 document.body.onload = setTimeout(() => {
     let toHide = document.body.querySelectorAll(".toBeHiddenAfterLoad");
     for (el of toHide){
         el.style.display = 'none';
     }
-}, 3000)
+
+    afterLoad();
+
+}, 1000)
 
